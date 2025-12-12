c9ff85658907132a9b7aeb6a003651d2cd2f6d3e
huzicuvas <huzaifacuvas@gmail.com>
2025-12-13 04:57:04 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Example JS file (you can add more functionality later)
+console.log("Website Loaded Successfully");
+
+// Simple reset confirmation
+function confirmReset() {
+    return confirm("Are you sure you want to reset the form?");
+}
